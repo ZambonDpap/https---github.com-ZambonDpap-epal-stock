bad7ebcace2ecc0f3ab79fb55e9069c6843cc988
stef merl <smerliao@gmail.com>
2023-07-26 01:01:46 +0300

    PAOK re

--- home.php
+++ home.php
@@ -6,7 +6,7 @@
     <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 
     <head>
-       <title>Βρές ένα όνομα</title>
+       <title>Βρές ένα όνομα 123</title>
        <!-- Load jquery 3.7.0 from the CDN -->
        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 
